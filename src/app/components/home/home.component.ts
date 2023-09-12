5ad7eb76fd030437113d8da87e79bd76d200e7bc
Sebastian Abalos <arnaldosebastianabalos@gmail.com>
2023-09-12 19:38:43 -0300

    todas las estructuras

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-card01: string ="../../assets/fondo640.jpg"
+card01: string ="../../../assets/fondo640.jpg"
 card02: string ="../../../assets/fondo6401.jpg"
 card03: string ="../../../assets/fondo6402.jpg"
 card04: string ="../../../assets/fondo6403.jpg"
